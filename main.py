ffa89dfd3f71562a896352a12f5aa786681cf0af
MahfuzKhandaker <mahfuzkhandaker.bd@gmail.com>
2022-10-27 01:47:26 +0600

    another commit

--- main.py
+++ main.py
@@ -2,15 +2,30 @@ from selenium import webdriver
 from selenium.webdriver.chrome.service import Service as ChromeService
 from selenium.webdriver.chrome.options import Options
 from webdriver_manager.chrome import ChromeDriverManager
-
-
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
 
 options = Options()
 options.add_experimental_option("excludeSwitches", ["enable-logging"])
 options.add_experimental_option("detach", True)
 
 driver = webdriver.Chrome(options=options, service=ChromeService(ChromeDriverManager().install()))
-
-driver.get('https://www.google.com/')
+# Navigate to url
+driver.get('https://www.google.com')
 
 driver.maximize_window()
+print(driver.title)
+
+# Store 'SearchInput' element
+SearchInput = driver.find_element(By.NAME, "q")
+SearchInput.send_keys("selenium")
+
+SearchInput.send_keys(Keys.RETURN)
+
+print(driver.current_url)
+# Clears the entered text
+SearchInput.clear()
+
+
+
+driver.close()
